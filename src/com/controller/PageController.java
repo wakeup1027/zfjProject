3e92cfacaf46bad391f6054de892d29075d94f60
wakeup1027 <446113922@qq.com>
2017-11-17 15:23:13 +0800

    增加

--- src/com/controller/PageController.java
+++ src/com/controller/PageController.java
@@ -13,4 +13,32 @@ public class PageController extends Controller {
 	public void kehuan(){
 		render("/kehuan.html");
 	}
+	
+	public void zainan(){
+		render("/zainan.html");
+	}
+	
+	public void dongzuo(){
+		render("/dongzuo.html");
+	}
+	
+	public void xiju(){
+		render("/xiju.html");
+	}
+	
+	public void xuanyi(){
+		render("/xuanyi.html");
+	}
+	
+	public void kongbu(){
+		render("/kongbu.html");
+	}
+	
+	public void aiqing(){
+		render("/aiqing.html");
+	}
+	
+	public void donghua(){
+		render("/donghua.html");
+	}
 }
